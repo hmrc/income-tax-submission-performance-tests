ea74de1205d90f59b3990a315987fef7d687e8e9
Robert Webb <28566263+robert-webb-hmrc@users.noreply.github.com>
2021-02-01 13:31:59 +0000

    SASS-233 : URL update for taxed/untaxed interest details id

--- src/test/scala/uk/gov/hmrc/perftests/itsass/InterestRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/itsass/InterestRequests.scala
@@ -19,7 +19,7 @@ package uk.gov.hmrc.perftests.itsass
 import io.gatling.http.Predef.{http, status}
 import io.gatling.core.Predef._
 import uk.gov.hmrc.performance.conf.ServicesConfiguration
-import uk.gov.hmrc.perftests.itsass.IncomeTaxSubmissionRequests.{personalIncomeBaseUrl, saveCsrfToken}
+import uk.gov.hmrc.perftests.itsass.IncomeTaxSubmissionRequests.{personalIncomeBaseUrl, saveCsrfToken, saveRedirectPath}
 
 object InterestRequests extends ServicesConfiguration {
   val interestUrl: String = personalIncomeBaseUrl + "/income-through-software/return/personal-income/2022/interest"
@@ -33,15 +33,16 @@ object InterestRequests extends ServicesConfiguration {
     .post(s"$interestUrl/untaxed-uk-interest")
     .formParam("""csrfToken""", """${csrfToken}""")
     .formParam("yes_no", "yes")
+    .check(saveRedirectPath())
     .check(status.is(303))
 
   def getUntaxedUKInterestDetailsPage = http("Get Untaxed UK Interest Details Page")
-    .get(s"$interestUrl/untaxed-uk-interest-details")
+    .get(personalIncomeBaseUrl + "${redirectPath}": String)
     .check(saveCsrfToken)
     .check(status.is(200))
 
   def postUntaxedUKInterestDetailsPage = http("Post Untaxed UK Interest Details Page")
-    .post(s"$interestUrl/untaxed-uk-interest-details")
+    .post(personalIncomeBaseUrl + "${redirectPath}": String)
     .formParam("""csrfToken""", """${csrfToken}""")
     .formParam("untaxedAccountName", "Tesco")
     .formParam("untaxedAmount", "1000")
@@ -66,15 +67,16 @@ object InterestRequests extends ServicesConfiguration {
     .post(s"$interestUrl/taxed-uk-interest")
     .formParam("""csrfToken""", """${csrfToken}""")
     .formParam("yes_no", "yes")
+    .check(saveRedirectPath())
     .check(status.is(303))
 
   def getTaxedUKInterestDetailsPage = http("Get Taxed UK Interest Details Page")
-    .get(s"$interestUrl/taxed-uk-interest-details")
+    .get(personalIncomeBaseUrl + "${redirectPath}": String)
     .check(saveCsrfToken)
     .check(status.is(200))
 
   def postTaxedUKInterestDetailsPage = http("Post Taxed UK Interest Details Page")
-    .post(s"$interestUrl/taxed-uk-interest-details")
+    .post(personalIncomeBaseUrl + "${redirectPath}": String)
     .formParam("""csrfToken""", """${csrfToken}""")
     .formParam("taxedAccountName", "Tesco")
     .formParam("taxedAmount", "1000")
