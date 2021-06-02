002e4ed51faf167f43fd2c5ae7536d185a899079
Joey Moore <22170468+joeyymoore@users.noreply.github.com>
2021-06-02 15:49:20 +0100

    added employment flows

--- src/test/scala/uk/gov/hmrc/perftests/itsass/IncomeTaxSubmissionSimulation.scala
+++ src/test/scala/uk/gov/hmrc/perftests/itsass/IncomeTaxSubmissionSimulation.scala
@@ -20,13 +20,14 @@ import uk.gov.hmrc.performance.simulation.PerformanceTestRunner
 import uk.gov.hmrc.perftests.itsass.AuthLoginRequests._
 import uk.gov.hmrc.perftests.itsass.IncomeTaxSubmissionRequests._
 import uk.gov.hmrc.perftests.itsass.DividendsRequests._
+import uk.gov.hmrc.perftests.itsass.EmploymentsRequests._
 import uk.gov.hmrc.perftests.itsass.InterestRequests._
 
 class IncomeTaxSubmissionSimulation extends PerformanceTestRunner {
 
   setup("individual-dividends", "Individual Dividends Journey") withRequests(
     getLoginPage,
-    postIndividualLoginPage,
+    postIndividualLoginPage("AA111111A"),
     getStartPage,
     getOverviewPage,
     getUKDividendsStatusPage,
@@ -43,7 +44,7 @@ class IncomeTaxSubmissionSimulation extends PerformanceTestRunner {
 
   setup("agent-dividends", "Agent Dividends Journey") withRequests(
     getLoginPage,
-    postAgentLoginPage,
+    postAgentLoginPage("AA111112A"),
     getInsertAdditionalParametersEndPoint,
     getStartPage,
     getOverviewPage,
@@ -61,7 +62,7 @@ class IncomeTaxSubmissionSimulation extends PerformanceTestRunner {
 
   setup("individual-interest", "Individual Interest Journey") withRequests(
     getLoginPage,
-    postIndividualLoginPage,
+    postIndividualLoginPage("AA111111A"),
     getStartPage,
     getOverviewPage,
     getUntaxedUKInterestStatusPage,
@@ -98,7 +99,7 @@ class IncomeTaxSubmissionSimulation extends PerformanceTestRunner {
 
   setup("agent-interest", "Agent Interest Journey") withRequests(
     getLoginPage,
-    postAgentLoginPage,
+    postAgentLoginPage("AA111112A"),
     getInsertAdditionalParametersEndPoint,
     getStartPage,
     getOverviewPage,
@@ -132,6 +133,29 @@ class IncomeTaxSubmissionSimulation extends PerformanceTestRunner {
     postTaxedUKInterestSummaryPage,
     getInterestCheckYourAnswersPage,
     postInterestCheckYourAnswersPage
+  )
+
+  setup("individual-employment", "Individual Single Employment Journey") withRequests(
+    getLoginPage,
+    postIndividualLoginPage("AA133742A"),
+    getStartPage,
+    getOverviewPage,
+    getEmploymentSummaryPage,
+    getEmploymentDetailsPage("00000000-0000-1000-8000-000000000000"),
+    getEmploymentBenefitsPage("00000000-0000-1000-8000-000000000000"),
+    getEmploymentExpensesPage
+  )
+
+  setup("agent-employment", "Agent Multiple Employments Journey") withRequests(
+    getLoginPage,
+    postAgentLoginPage("BB444444A"),
+    getStartPage,
+    getOverviewPage,
+    getEmploymentSummaryPage,
+    getEmployerDetailsAndBenefitsPage("00000000-5555-0000-0000-000000000001"),
+    getEmploymentDetailsPage("00000000-5555-0000-0000-000000000001"),
+    getEmploymentBenefitsPage("00000000-5555-0000-0000-000000000001"),
+    getEmploymentExpensesPage
   )
 
   runSimulation()
