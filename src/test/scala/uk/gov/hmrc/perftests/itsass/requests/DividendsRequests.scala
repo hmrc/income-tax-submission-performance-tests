12301448826ae850d6081a9eb8302a1d8a18502b
SamuelTwumBarima <93492001+SamuelTwumBarima@users.noreply.github.com>
2022-07-25 14:57:34 +0100

    fixed failing test, Crystallisation and Dividends
    fixed failing test, Crystallisation and Dividends

--- src/test/scala/uk/gov/hmrc/perftests/itsass/requests/DividendsRequests.scala
+++ src/test/scala/uk/gov/hmrc/perftests/itsass/requests/DividendsRequests.scala
@@ -25,12 +25,12 @@ import uk.gov.hmrc.perftests.itsass.requests.RequestsHelper._
 object DividendsRequests extends ServicesConfiguration {
 
   def getDividendsGatewayPage: HttpRequestBuilder = http("Get Dividends Gateway Page")
-    .get(s"$interestUrl/interest-from-UK")
+    .get(s"$dividendsUrl/dividends-from-stocks-and-shares")
     .check(saveCsrfToken())
     .check(status.is(200))
 
   def postDividendsGatewayPage: HttpRequestBuilder = http("Post Dividends Gateway Page")
-    .post(s"$interestUrl/interest-from-UK")
+    .post(s"$dividendsUrl/dividends-from-stocks-and-shares")
     .formParam("""csrfToken""", """${csrfToken}""")
     .formParam("value", true)
     .check(status.is(303))
